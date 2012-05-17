6655b26d286de79162319100953842c101e5adbd
Jim Denton <inkpen1986@me.com>
2012-05-17 01:53:19 -0600

    Create opportunity form with acceptance test

--- routes/opportunity.rb
+++ routes/opportunity.rb
@@ -2,4 +2,12 @@ class Rook < Sinatra::Base
   get '/opportunity' do
     haml :opportunity
   end
+
+  post '/opportunity' do
+    @title = params[:title] 
+    @skills = params[:skills]
+    @description = params[:description]
+
+    haml :opportunity
+  end 
 end
--- spec/acceptance/create_opportunity_spec.rb
+++ spec/acceptance/create_opportunity_spec.rb
@@ -1,16 +1,30 @@
 require_relative '../spec_helper'
 
-describe "Opportunity page" do
+describe "Creating an Opportunity" do
   
   include AcceptanceHelper
 
-  it "returns 200" do
+  before(:each) do
     visit "/opportunity"
+  end
+
+  it "returns 200" do
     assert_equal 200, page.status_code
   end
 
   it "has a header" do
-    visit "/opportunity"
     assert has_content?("Create an Opportunity")
+  end
+
+  it "has a title field" do
+   fill_in('Title', :with => 'Ruby for Beginners')
+  end
+  
+  it "has a skills field" do
+    fill_in('Skills', :with => 'Ruby')
+  end
+
+  it "has a description field" do
+    fill_in('Description', :with => 'Learn Ruby from the basics')
   end
 end

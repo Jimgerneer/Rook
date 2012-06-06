b06ed46c7ec3720a6ce23ddcdc6e8af928649105
Jim Denton <inkpen1986@me.com>
2012-06-06 11:37:17 -0600

    Fixed bad merge

--- models/subscription.rb
+++ models/subscription.rb
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
-class Rook
-=begin
   class Subscription
+  end
+    
+=begin
     include DataMapper::Resource
     belongs_to :user, :key => true
     belongs_to :person, :key =>true
-  end
 =end
-end
 
-    
-=======
-class Subscription
-=begin
-  include DataMapper::Resource
-  
-  belongs_to :user, :key => true
-  belongs_to :opportunities, :key => true
-=end
-end
->>>>>>> Namespace

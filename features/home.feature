1e77168f842d19a5f28c93362b2cc6cec6d027e1
Jim Denton <inkpen1986@me.com>
2012-08-09 17:54:36 -0600

    Finished moving acceptance tests, added opp Update

--- features/home.feature
+++ features/home.feature
@@ -3,3 +3,19 @@ Feature: view pages
   Scenario: Home page
     Given I am viewing "/"
     Then I should see "Welcome to Rook"
+
+  Scenario: Visit login
+    Given I am viewing "/"
+    And I click the "Login" link
+    Then I should be on "/login" 
+
+  Scenario: Visit Sign up
+    Given I am viewing "/"
+    And I click the "Sign-Up" link
+    Then I should be on "/signup"
+
+  Scenario: Opportunities paginate
+    Given 11 valid opportunites are created
+    And I am viewing "/"
+    Then I should see "Available Opportunities!"
+    And there should only be 10 opportunities
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,7 +1,22 @@
-Given /^I sign in as "([^\"]*)"$/ do |user|
-  User.gen {{ :username => user }}
-  fill_in('username', :with => user)
+Given /^I am signed in with a valid user$/ do
+  user = User.gen
+  visit ('/login')
+  fill_in('username', :with => user.username)
   fill_in('password', :with => 'doobar')
+  click_button('Submit')
+end
+
+Given /^I am on the login page$/ do
+  visit "/login"
+end
+
+Given /^I am on the signup page$/ do
+  visit "/signup"
+end
+
+Given /^I try to log in with an uncreated user$/ do
+  fill_in('username', :with => 'VeryRandomName')
+  fill_in('password', :with => 'nope')
   click_button('Submit')
 end
 
@@ -14,16 +29,62 @@ Given /^I am on the opportunities page$/ do
   visit "/"
 end
 
+Given /^I am on the create opportunity page$/ do
+  visit "/opportunity"
+end
+
+Given /^I am on the update opportunity page$/ do
+  visit "/opportunity/edit.#{@current_opportunity.id}"
+end
+
+Given /^I fill in the fields$/ do |table|
+  @current_form = table.rows_hash
+  @current_form.each {|field, value| fill_in field, :with => value }
+end
+
+Given /^I click the submit button$/ do
+  click_button('Submit')
+end
+
+Given /^I click the "([^|"]*)" link$/ do |link|
+  click_link(link)
+end
+
 Given /^I click the "([^\"]*)" button$/ do |button|
   within(:xpath, '//tbody/tr//td/form')do
     click_button(button)
   end
 end
 
-Then /^the opportunity should be booked$/ do
-  assert_equal 5, Booking.count
+Given /^11 valid opportunites are created$/ do
+  11.times { User.gen(:opp) }
 end
 
-Then /^I should be on the user page$/ do
-  assert_equal "/user", page.current_path
+Given /^I am signed in as a user that has authored an opportunity$/ do
+  User.gen(:opp)
+  @current_opportunity = Opportunity.last
+end
+
+Then /^the opportunity should be updated$/ do
+  assert_equal @current_form["Title:"], Opportunity.last.title 
+end
+
+Then /^the opportunity should be booked$/ do
+  assert_equal 1, Booking.count
+end
+
+Then /^I should have signed up$/ do
+  assert_equal @current_form["Username:"], User.last.username
+end
+
+Then /^the opportunity should be created$/ do
+  assert_equal 1, Opportunity.count
+end
+
+Then /^I should be on "([^\"]*)"$/ do |url|
+  assert_equal url, page.current_path
+end
+
+Then /^there should only be 10 opportunities$/ do
+  assert_equal 10, page.all(:xpath, '//tbody/tr').length
 end

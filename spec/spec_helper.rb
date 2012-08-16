b1022d27cc77b030e9e3d272d2ada404c5ccab41
Jim Denton <inkpen1986@me.com>
2012-08-15 22:32:17 -0600

    Restored Database Cleaner and config for Heroku

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,22 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/spec'
 require 'mocha'
+require 'database_cleaner'
 
 ENV["RACK_ENV"] = "test"
 require_relative '../rook'
 
+DatabaseCleaner.strategy = :transaction
+class MiniTest::Spec
+  before :each do
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+end
+
 MiniTest::Unit.runner = MiniTest::SuiteRunner.new
 MiniTest::Unit.runner.reporters << MiniTest::Reporters::ProgressReporter.new
 require_relative 'spec_fixtures'
-
-def sign_in(user)
-  visit "/login"
-  fill_in('username', :with => 'DecoyDrone')
-  fill_in('password', :with => 'doobar')
-  click_button('Submit')
-end

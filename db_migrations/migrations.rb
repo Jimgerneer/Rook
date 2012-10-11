9dc0e0ac4d2be00d8b25db1e82eaa40ba4c10139
Jim Denton <inkpen1986@me.com>
2012-10-11 12:54:10 -0600

    DM migration script to fix User bio column

---
+++ db_migrations/migrations.rb
@@ -0,0 +1,17 @@
+require 'dm-migrations'
+require 'dm-migrations/migration_runner'
+
+DataMapper.setup(:default, 'postgres://username:password@ec2-107-20-224-107.compute-1.amazonaws.com:5432/dd74kte6km9s1d')
+
+DataMapper::Logger.new(STDOUT, :debug)
+DataMapper.logger.debug( "Starting Migration" )
+
+migration 1, :user_bio_to_text do
+  up do
+    modify_table :users do
+      change_column :bio, 'text'
+    end
+  end
+end
+
+migrate_up!
